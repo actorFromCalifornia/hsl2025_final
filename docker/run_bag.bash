2eda5646c37d0e73d8a4ed5af59e4b5e1107007b
Nikita Savinov <nikita.savinov@corp.mail.ru>
2025-11-01 11:04:57 +0300

    some updates with rtabmap + updates with odom from robot

--- docker/run_bag.bash
+++ docker/run_bag.bash
@@ -7,4 +7,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BRINGUP_MODE="bag"
 echo "[docker] Starting bringup stack (mode=${BRINGUP_MODE})"
 
-docker compose -f "${SCRIPT_DIR}/docker-compose.yml" --profile bag up --force-recreate "$@"
+docker compose -f "${SCRIPT_DIR}/docker-compose.yml" --profile bag --profile foxglove up --force-recreate "$@"
