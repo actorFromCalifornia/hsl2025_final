80bde863a467abdb41c0d79664362577d34c9d1d
Nikita Savinov <nikita.savinov@corp.mail.ru>
2025-11-02 11:11:44 +0300

    add timer

--- launch/bringup_common.launch.py
+++ launch/bringup_common.launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
@@ -67,7 +67,12 @@ def generate_launch_description():
                 'use_sim_time': use_sim_time,
             }],
         ),
-        IncludeLaunchDescription(
-            coverage_launch,
+        TimerAction(
+            period=10.0,
+            actions=[
+                IncludeLaunchDescription(
+                    coverage_launch,
+                ),
+            ],
         ),
     ])
